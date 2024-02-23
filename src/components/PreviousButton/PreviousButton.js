35d4853d414d088f80418e18c561b90906cd9a94
Devin <devster99@gmail.com>
2024-02-22 22:42:39 -0700

    updated previousButton logic

--- src/components/PreviousButton/PreviousButton.js
+++ src/components/PreviousButton/PreviousButton.js
@@ -2,16 +2,18 @@ import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 
 const PreviousButton = () => {
-    const navigate = useNavigate();
-    const {page} = useParams();
-  
-    const handleClick = () => {
-      navigate(`/Results/${page - 1}`)
-    };
-    return (
-      <div>
-        <button onClick={() => handleClick()}>Previous</button>
-      </div>
-    );
-}
+  const navigate = useNavigate();
+  const { page } = useParams();
+
+  const handleClick = () => {
+    if (page === 1) navigate(`/RevVIN`);
+    else navigate(`Results/${page - 1}`);
+  };
+
+  return (
+    <div>
+      <button onClick={() => handleClick()}>Previous</button>
+    </div>
+  );
+};
 export default PreviousButton;
